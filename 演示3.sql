d2e5279a63e464f9da3ebeffd3a17e2d56b0ae71
fxh <fenghuan204@gmail.com>
2022-05-23 17:57:58 +0800

    20220523

--- 演示3.sql
+++ 演示3.sql
@@ -1,4 +1,12 @@
-﻿select *
-          from yhq.cq_yhq_combo_config@to_air
-         where  valid_flag = 1
-         and expiration_date=date'2022-06-30'
+﻿select flights_city_name,count(1)
+from(
+select distinct segment_code,flights_city_name,flights_segment_name
+         from dw.dim_segment_type
+         where flights_segment_name is not null)
+ group by flights_city_name
+
+
+ select * from if.v_rep_segment_varicost
+
+
+ select count(1),count(distinct flightno||flights_segment_name) from if.v_rep_segment_varicost
